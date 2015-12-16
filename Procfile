07828345509684f14c816cc372853273353d09f6
Boris Bügling <boris@icculus.org>
2015-12-16 13:59:57 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./versions $PORT
---
+++ versions.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+VERSIONS=`curl -s 'https://swift.org/download/'|grep 'osx.pkg'|sed -e 's_.*/swift-\(.*\)-osx.pkg.*_\1_'`
+
+if [ -z "$1" ]
+then
+	echo "Usage: $0 port"
+	exit 1
+fi
+
+while true
+do
+	if [ `uname` == "Darwin" ]
+	then
+		echo "\n$VERSIONS"|nc -l localhost $1
+	else
+		echo -e "\n$VERSIONS"|nc -l -p $1
+	fi
+
+	if [ $? -ne 0 ]
+	then
+		break
+	fi
+done
